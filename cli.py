3baedccd1ad025318173c25fdb83998a421997db
TeaOfTheSea <coltipto@ttu.edu>
2021-12-06 20:12:01 -0600

    Created interface for adding more calibration data. Also rewrote csv imports for both files to just expect the csv to be in the same directory because it made my life easier.

--- cli.py
+++ cli.py
@@ -4,13 +4,21 @@ import sys
 import matplotlib.pyplot as plt
 import numpy as np
 
+def calibrate(data):
+    if len(sys.argv[1:]) == 4:
+        #import the PressureSensorData
+        sensorData = pd.read_csv('PressureSensorData.csv')
+        
+        #update it
+        sensorData.loc[-1] = [data[0], data[1], data[2], data[3]]
+        sensorData.to_csv('PressureSensorData.csv', index=False)
+        
+    #elif len(sys.argv[1:]) == 3:
+    else: print('Invalid usage (use -h for help)')
 #math to generate numbers used for calculation
 def theMath():
     #import the PressureSensorData
-    if sys.platform == 'darwin': #for Patrick's Mac because Tim Cook runs a terrible company
-        sensorData = pd.read_csv('PressureSensorData.csv')
-    else:
-        sensorData = pd.read_csv('../PressureSensorData.csv')
+    sensorData = pd.read_csv('PressureSensorData.csv')
 
     #reformat the data
     sensorData.columns = ['testID', 'truePressure', 'analogPressure', 'voltage'] #renaming columns for easier understanding
@@ -39,19 +47,21 @@ def theMath():
 
 #Actual CLI arguments
 if len(sys.argv[1:]) == 0:
-	print('Invalid usage (use -h for help)')
+    print('Invalid usage (use -h for help)')
 else:
-	opt=sys.argv[1:][0]
-	if len(sys.argv[1:]) == 2:
-		apvr, apvSlopeOU, apvInterOU, voltagestd, analogstd = theMath()
-		num = float(sys.argv[1:][1])
-		if opt == '-a' or opt == '--analog':
-			print('We are 95% confident that the voltage is within ' + str(apvSlopeOU*num+apvInterOU-2*voltagestd) + ' and ' + str(apvSlopeOU*num+apvInterOU+2*voltagestd) + '.')
-		elif opt == '-v' or opt == '--voltage':
-			print('We are 95% confident that the analog reading is within ' + str(((num-apvInterOU)/apvSlopeOU)-2*analogstd) + ' and ' + str(((num-apvInterOU)/apvSlopeOU)+2*analogstd) + '.')
-		else:
-			print('Invalid option (use -h for help)')
-	elif opt == '-h' or opt == '--help':
-		print('Usage: python cli.py [OPTION] [VALUE]\n\nAvailable options:\n  -a, --analog; Converts analog data to voltage reading\n  -v, --voltage; Converts voltage reading to analog data\n  -h, --help; Displays this menu')
-	else:
-		print('Invalid usage (use -h for help)')
+    opt=sys.argv[1:][0]
+    if(opt == '-c' or opt == '--calibrate'):
+        calibrate(sys.argv[1:])
+    elif len(sys.argv[1:]) == 2:
+        apvr, apvSlopeOU, apvInterOU, voltagestd, analogstd = theMath()
+        num = float(sys.argv[1:][1])
+        if opt == '-a' or opt == '--analog':
+            print('We are 95% confident that the voltage is within ' + str(apvSlopeOU*num+apvInterOU-2*voltagestd) + ' and ' + str(apvSlopeOU*num+apvInterOU+2*voltagestd) + '.')
+        elif opt == '-v' or opt == '--voltage':
+            print('We are 95% confident that the analog reading is within ' + str(((num-apvInterOU)/apvSlopeOU)-2*analogstd) + ' and ' + str(((num-apvInterOU)/apvSlopeOU)+2*analogstd) + '.')
+        else:
+            print('Invalid option (use -h for help)')
+    elif opt == '-h' or opt == '--help':
+        print('Usage: python cli.py [OPTION] [VALUE(S)]\n\nAvailable options:\n  -a, --analog; Converts analog data to voltage reading\n  -v, --voltage; Converts voltage reading to analog data\n  -c, --calibrate; Provide a True Pressure, an Analog Pressure, and a Voltage\n  -h, --help; Displays this menu')
+    else:
+        print('Invalid usage (use -h for help)')
